1ad33ca9530da498a8b3614954ff4b9afdd2a4ec
Ben <careydas.himself@gmail.com>
2022-06-24 17:38:00 +0200

    1.0

---
+++ app.py
@@ -0,0 +1,54 @@
+import json, config
+from flask import Flask, request, jsonify, render_template
+from binance.client import Client
+from binance.enums import *
+from math import *
+
+
+app = Flask(__name__)
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+
+def order(side, quantity, symbol, order_type=FUTURE_ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    #print(request.data)
+    data = json.loads(request.data)
+    
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid passphrase"
+        }
+    
+    symbol= data['ticker'][:-4] #new
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['market_position_size'] #new
+    order_response = order(side, quantity, symbol) #new
+    #print(symbol)
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
---
+++ config.py
@@ -0,0 +1,4 @@
+WEBHOOK_PASSPHRASE = "blalb" 
+
+API_KEY = 'xoxoxoxoxo'
+API_SECRET = 'xoxoxoxoxox'
